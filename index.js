db8572fe587beaaa87af581bc044bc1039fc9662
ustunsel <117071676+ustunsel@users.noreply.github.com>
2024-02-18 15:07:09 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World');
+});
+
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => console.log(`Server running on port ${port}`));
